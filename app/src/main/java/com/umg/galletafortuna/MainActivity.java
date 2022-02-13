f3bbedfc2d7bf5e2802ad84887d6764415bada3c
Kevinisky27 <keare27@gmail.com>
2022-02-13 12:51:20 -0600

    Integración con POO

--- app/src/main/java/com/umg/galletafortuna/MainActivity.java
+++ app/src/main/java/com/umg/galletafortuna/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
 
     Button btnMensaje;
     TextView tvMensaje;
@@ -24,32 +25,14 @@ public class MainActivity extends AppCompatActivity {
         btnMensaje = findViewById(R.id.fortuneCookie);
         tvMensaje = findViewById(R.id.tvMensaje);
 
-        final String fortuna [] = {
-            "Tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.",
-            "Concéntrate en lo que quieres lograr y ganaras. No lo olvides.",
-            "El cielo sera tu limite, pues grandes acontecimientos te sucederán.",
-            "Te sentirás feliz como un niño y veras al mundo con sus ojos.",
-            "Vivirás tu vejez con comodidades y riquezas materiales.",
-            "Confía en tu suerte, que es mucha y te rodeara de prosperidad.",
-            "No todo el mundo puede recibir las mismas cosas. Se practico.",
-            "Hoy es el momento de explorar: no temas.",
-            "Muy pronto seras incluido en muchas reuniones, fiestas y tertulias.",
-            "Cuando busques lo que mas deseas, recuerda hacer tu mejor esfuerzo.",
-            "Tienes por delante un maravilloso día para triunfar; disfrútalo y compártelo.",
-            "Hoy seras reconocido por tus dones especiales y lograras ser feliz por muchas horas.",
-            "Tu corazón estallara de alegría con la llegada de buenas noticias.",
-            "Mañana puede ser muy tarde para disfrutar lo que tienes hoy.",
-            "Seras promovido en tu trabajo debido a tus logros y capacidades."
-        };
-
 
         btnMensaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random random = new Random();
-                int galleta = random.nextInt(15);
+                MensajesMotivadores mensaje = new MensajesMotivadores();
 
-                tvMensaje.setText(fortuna[galleta]);
+                int galleta = random.nextInt(15);
+                tvMensaje.setText(mensaje.fortuna[galleta]);
 
                 Toast.makeText(getApplicationContext(), "Galleta Fabricada", Toast.LENGTH_LONG).show();
             }
